fab9c1ccef4ba3ed4a64058d4c57a47d6049a9e2
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-04 17:12:23 +0000

    comparisons

---
+++ Basics/tut5-comparisons.js
@@ -0,0 +1,13 @@
+console.log("2">1);
+console.log("2">2);
+console.log("2">=2);
+console.log("2">3);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null===0); // it checks value along with datatype also
+console.log(null>=0);
+
+console.log(null>undefined);
+console.log(null>=undefined);
+console.log(null==undefined);
