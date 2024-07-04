d2905cdefc4843c4848c37278ed984d2de299c2d
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-04 15:58:01 +0000

    some more changes

---
+++ Basics/tut2-variables.js
@@ -0,0 +1,12 @@
+const myId=12;
+let myName="Shubham";
+var myHeight=178; /* dont use it because of scope issues */
+shoeSize=9;        /* not a good practice */
+
+// myId=19; can't be changes as const
+myName="Viom";
+
+console.log(myId);
+console.log(myName);
+
+console.table([myId,myName]); /* Another way to write in one line */
