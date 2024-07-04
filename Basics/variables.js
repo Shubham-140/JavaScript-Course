3022aff3deae4d6bb228a76d0c82011c305efbc1
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-04 13:51:53 +0000

    Variables names changed

---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const myId=12;
+let myName="Shubham";
+
+// myId=19; can't be changes as const
+myName="Viom";
+
+console.log(myId);
+console.log(myName);
+
+console.table([myId,myName]);  /* Another way to write in one line */
