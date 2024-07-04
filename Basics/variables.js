423231ea6fbfe64bf40cb0e79b2689c211875d1e
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-04 15:49:59 +0000

    some more changes

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,5 +1,7 @@
 const myId=12;
 let myName="Shubham";
+var myHeight=178; /* dont use it because of scope issues */
+shoeSize9;        /* not a good practice */
 
 // myId=19; can't be changes as const
 myName="Viom";
