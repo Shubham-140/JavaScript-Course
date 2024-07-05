2c395f3d66b2e75ad700045dba863f9bd1892b57
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-05 17:53:33 +0000

    time and date object

---
+++ Basics/tut10-date.js
@@ -0,0 +1,17 @@
+let date=new Date();
+let myDate=new Date(2020,11,16,5,11,34);  //yr mon dt hr min sec
+let timeStamp=Date.now();
+
+console.log(date); // shows in different format
+console.log(date.getMonth()+1);
+console.log(date.getDate());
+
+console.log(date.toString()); // shows in normal format
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+console.log(timeStamp); // gives ms from 1st jan 1970
+console.log(Math.floor(timeStamp/1000)); // ms to sec
