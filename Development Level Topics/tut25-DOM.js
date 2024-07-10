99e93d60786b6e3fd5bd317ad9202dc2d822c80f
Shubham-140 <121750540+Shubham-140@users.noreply.github.com>
2024-07-10 10:31:14 +0000

    JS

---
+++ Development Level Topics/tut25-DOM.js
@@ -0,0 +1,42 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="./style.css">
+//     <title>Background Color Changer</title>
+// </head>
+// <body>
+//     <h1>Background Color Changer</h1>
+//     <div class="buttons">
+//         <button class="color-button" id="red"></button>
+//         <button class="color-button" id="blue"></button>
+//         <button class="color-button" id="green"></button>
+//         <button class="color-button" id="yellow"></button>
+//         <button class="color-button" id="olive"></button>
+//         <button class="color-button" id="pink"></button>
+//     </div>
+//     <h3>Tap on the button to change the color you want!!</h3>
+// </body>
+// <script src="./script.js"></script>
+// </html>
+
+
+
+// We can access the html elements by using JS
+
+// Using ID
+const yellowCol=document.getElementById('yellow');
+// Now we can modify the element having id='yellow' and even we can change the events wrt this element
+
+// Similarly,there is a method for accessing element through class
+const button=document.getElementsByClassName('buttons');
+
+
+// There is one more way to do it
+yellowCol=document.querySelector('yellow');
+
+
+// If there are many elements inside a element container, we can use 
+const nothing=document.querySelectorAll('imaginary');
+// Now, if we want to access the data inside,we can use forEach loop or if only one element, then use indices.
