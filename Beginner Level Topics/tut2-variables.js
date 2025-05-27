b30a6feee31991d1f9c089e931a347f2d82680ca
Shubham140 <Shubham140@gmail.com>
2025-05-27 23:40:46 +0530

    added some more notes

--- Beginner Level Topics/tut2-variables.js
+++ Beginner Level Topics/tut2-variables.js
@@ -1,21 +1,31 @@
 const myId=12;
 let myName="Shubham";
-<<<<<<< HEAD
+
 var myHeight=178; /* dont use it because of scope issues as it has function level scope */ 
 shoeSize=9;        /* not a good practice, its a global variable and works in non strict mode */
 
 // Both let and const have block level scope whereas var has a functional scope
 
 // myId=19; // cant be changes as const
-=======
+
 var myHeight=178; /* dont use it because of scope issues */
 shoeSize=9;        /* not a good practice */
 
 // myId=19; can't be changes as const
->>>>>>> 1b5926f56fde4d0a13f71b9fcfb3ef3b1518bccd
 myName="Viom";
 
 console.log(myId);
 console.log(myName); 
 
 console.table([myId,myName]); /* Another way to write in one line */
+
+
+
+// | Feature                          | `var`                      | `let`                      | `const`                    |
+// |----------------------------------|----------------------------|----------------------------|----------------------------|
+// | **Scope**                        | Function-scoped            | Block-scoped               | Block-scoped               |
+// | **Hoisting**                     | Hoisted (undefined)        | Hoisted (TDZ error)        | Hoisted (TDZ error)        |
+// | **Redeclaration & Reassignment** | Allowed                    | Only Reassignment Allowed  |  Not allowed               |
+// | **Use Case**                     | Legacy code                | Mutable variables          | Constants                  |
+// | **Modern JS Recommendation**     | Not recommended            | Recommended                | Recommended                |
+// | **Example**                      | `var x = 10;`              | `let y = 20;`              | `const z = 30;`            |
